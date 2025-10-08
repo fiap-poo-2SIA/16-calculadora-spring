72f9dd53223da935698c597aeea67ba46a1ad332
Laboratório FIAP <marcos.selmini@uol.com.br>
2025-10-08 09:42:43 -0300

    projeto calculadora

---
+++ src/main/java/br/fiap/calculadora_spring/controller/CalculadoraController.java
@@ -0,0 +1,29 @@
+package br.fiap.calculadora_spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.math.BigDecimal;
+
+@Controller
+public class CalculadoraController {
+
+    private final CalculadoraService service;
+
+    public CalculadoraController(CalculadoraService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/calcular")
+    public String calcular(@RequestParam String valor1,
+                           @RequestParam String valor2,
+                           @RequestParam String operacao,
+                           Model model)
+    {
+        BigDecimal resultado = service.calcular(valor1, valor2, operacao);
+        model.addAttribute("resultado", resultado);
+        return "index";
+    }
+}
